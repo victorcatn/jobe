01a89d88a6e9048f70b666ef7c9a45e57b4b5762
Richard Lobb <richard.lobb@canterbury.ac.nz>
2016-02-02 22:04:21 +1300

    Make it unsafe again (how did I lose those changes?).

--- application/libraries/LanguageTask.php
+++ application/libraries/LanguageTask.php
@@ -72,7 +72,7 @@ abstract class Task {
     // the webserver or any of the jobe<n> users, running programs will be able
     // to hoover up other students' submissions.
     public function __construct($sourceCode, $filename, $input, $params) {
-        $this->workdir = tempnam("/home/jobe/runs", "jobe_");
+        $this->workdir = tempnam("/tmp", "jobe_");
         if (!unlink($this->workdir) || !mkdir($this->workdir)) {
             log_message('error', 'LanguageTask constructor: error making temp directory');
             throw new Exception("Task: error making temp directory (race error?)");
@@ -139,102 +139,28 @@ abstract class Task {
     // Sets $this->cmpinfo accordingly.
     public abstract function compile();
 
-    
-    // Find a currently unused jobe user account.
-    // Uses a shared memory segment containing one byte (used as a 'busy'
-    // boolean) for each of the possible user accounts.
-    // If no free accounts exist at present, the function sleeps for a
-    // second then retries, up to a maximum of 10 retries.
-    // Throws OverloadException if a free user cannot be found, otherwise 
-    // returns an integer in the range 0 to jobe_max_users - 1 inclusive.
-    private function getFreeUser() {
-        global $CI;
-
-        $numUsers = $CI->config->item('jobe_max_users');
-        $key = ftok(__FILE__, 'j');
-        $sem = sem_get($key);
-        $user = -1;
-        $retries = 0; 
-        while ($user == -1 && $retries < MAX_RETRIES) {
-            sem_acquire($sem);
-            $shm = shm_attach($key); 
-            if (!shm_has_var($shm, ACTIVE_USERS)) {
-                // First time since boot -- initialise active list
-                $active = array();
-                for($i = 0; $i < $numUsers; $i++) {
-                    $active[$i] = FALSE;
-                }
-                shm_put_var($shm, ACTIVE_USERS, $active);
-            }
-            $active = shm_get_var($shm, ACTIVE_USERS);
-            for ($user = 0; $user < $numUsers; $user++) {
-                if (!$active[$user]) {
-                    $active[$user] = TRUE;
-                    shm_put_var($shm, ACTIVE_USERS, $active);
-                    break;
-                }
-            }
-            shm_detach($shm);
-            sem_release($sem);
-            if ($user == $numUsers) {
-                $user = -1;
-                $retries += 1;
-                if ($retries < MAX_RETRIES) {
-                    sleep(1);
-                } else {
-                    throw new OverloadException();
-                }
-            }
-        }
-        return $user;
-    }
-    
-    
-    // Mark the given user number (0 to jobe_max_users - 1) as free.
-    private function freeUser($userNum) {
-        $key = ftok(__FILE__, 'j');
-        $sem = sem_get($key);
-        sem_acquire($sem);
-        $shm = shm_attach($key);
-        $active = shm_get_var($shm, ACTIVE_USERS);
-        $active[$userNum] = FALSE;
-        shm_put_var($shm, ACTIVE_USERS, $active);
-        shm_detach($shm);
-        sem_release($sem);        
-    }
-
-    
+       
     // Execute this task, which must already have been compiled if necessary
+    // This version has no resource controls (which is why this is the
+    // 'unsafe' branch)!
     public function execute() {
 
         try {
             // Establish all the parameters for the job run
             
-            $userId = $this->getFreeUser();
-            $user = sprintf("jobe%02d", $userId);
+            // Can't do anything with all the following in this unprotected
+            // version but leave the code there for future use
             $filesize = 1000 * $this->getParam('disklimit'); // MB -> kB
             $streamsize = 1000 * $this->getParam('streamsize'); // MB -> kB
             $memsize = 1000 * $this->getParam('memorylimit');
             $cputime = $this->getParam('cputime');
             $numProcs = $this->getParam('numprocs');
-            $sandboxCmdBits = array(
-                 "sudo " . dirname(__FILE__)  . "/../../runguard/runguard",
-                 "--user=$user",
-                 "--time=$cputime",         // Seconds of execution time allowed
-                 "--filesize=$filesize",    // Max file sizes
-                 "--nproc=$numProcs",       // Max num processes/threads for this *user*
-                 "--no-core",
-                 "--streamsize=$streamsize");   // Max stdout/stderr sizes
 
-            if ($memsize != 0) {  // Special case: Matlab won't run with a memsize set. TODO: WHY NOT!
-                $sandboxCmdBits[] = "--memsize=$memsize";
-            }
-            $allCmdBits = array_merge($sandboxCmdBits, $this->getRunCommand());
-            $cmd = implode(' ', $allCmdBits) . " >prog.out 2>prog.err";
+            $cmd = implode(' ', $this->getRunCommand());
+            $cmd .= " >prog.out 2>prog.err";
 
             // Set up the work directory and run the job
             $workdir = $this->workdir;
-            exec("setfacl -m u:$user:rwX $workdir");  // Give the user RW access
             chdir($workdir);
             file_put_contents('prog.cmd', $cmd);
 
@@ -270,11 +196,6 @@ abstract class Task {
         catch (Exception $e) {
             $this->result = Task::RESULT_INTERNAL_ERR;
             $this->stderr = $e->getMessage();
-        }
-
-        if (isset($userId)) {
-            exec("sudo /usr/bin/pkill -9 -u $user"); // Kill any remaining processes
-            $this->freeUser($userId);
         }
 
     }
